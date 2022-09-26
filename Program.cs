03a06efc3cc7277de0d3e502640b2eb45376a698
Novi4ok <job_search31@mail.ru>
2022-09-26 16:02:00 +0300

    Added a method for creating a string array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метод для строкового массива
+
+string[] CreateStringArray(int size)
+{
+    string[] newArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input " + i + " element of array: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateStringArray(a);
+ShowArray(myArray);
